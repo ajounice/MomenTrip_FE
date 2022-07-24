d33e9d090dd975a2c679d7ae0fa6092a85155584
su-lim <sujin.0xlim@gmail.com>
2022-07-24 15:56:41 +0900

    feat: create search-before page

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -11,7 +11,7 @@ const TagItem = ({ id, name, value }: { id: string; name: string; value: string 
                     id={id}
                     // TODO max wide로 태그 갯수 설정 -> 디스플레이 속성보고 맞춰서 하기
                     className={
-                        'text-sm text-center mx-1 px-2 shadow-sm h-8 bg-gray-100 focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-full'
+                        'text-sm text-center mr-2 px-2 mt-2 shadow-sm h-8 bg-gray-100 focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-full'
                     }
                 >
                     {value}
